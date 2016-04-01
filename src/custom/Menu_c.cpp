fcb6a9610a5e1f2dea5d70134087f8e1ab4410f2
Nobumitsu Takaoka <nmbits@gmail.com>
2016-04-01 23:33:06 +0000

    B::Menu::rbe__gc_free removes items before deleting object

--- src/custom/Menu_c.cpp
+++ src/custom/Menu_c.cpp
@@ -2,7 +2,8 @@
 #include "Menu.hpp"
 #include "MenuItem.hpp"
 #include "deleting.hpp"
-
+#include "debug.hpp"
+#include "registory.hpp"
 
 namespace rbe
 {
@@ -12,6 +13,23 @@ namespace rbe
 		void Deleting<BMenu, BMenuItem>(BMenu *o, BMenuItem *t)
 		{
 			o->RemoveItem(t);
+		}
+	}
+
+	namespace B
+	{
+		void Menu::rbe__gc_free(void *ptr)
+		{
+		    RBE_TRACE("BMenu::rb__gc_free");
+		    RBE_PRINT(("  ptr = %p\n", ptr));
+			ObjectRegistory::Instance()->Delete(ptr);
+			PointerOf<BMenu>::Class *obj =
+				static_cast<PointerOf<BMenu>::Class *>(ptr);
+			BMenu *menu = dynamic_cast<BMenu *>(obj);
+			if (!menu)
+				debugger("Menu::rbe__gc_free cast failed.");
+			menu->RemoveItems(0, menu->CountItems(), false);
+			delete menu;
 		}
 	}
 }
